bdfa15ac489935dff524aedacdee061fea0af4cd
Oleg Masnik <olegmasnik@gmail.com>
2015-05-25 22:46:58 +0300

    add general dao interface and him implementation.

---
+++ SportBlog/src/main/java/com/sport/blog/dao/GeneralDAO.java
@@ -0,0 +1,17 @@
+package com.sport.blog.dao;
+
+import java.util.List;
+
+public interface GeneralDAO<E> {
+	
+	public void addElement(E element);
+
+    public void updateElement(E element);
+
+    public E getElementByID(Long elementId);
+
+    public List<E> getAllElements();
+
+    public void deleteElement(E element);
+
+}
---
+++ SportBlog/src/main/java/com/sport/blog/daoImpl/GeneralDaoImpl.java
@@ -0,0 +1,82 @@
+package com.sport.blog.daoImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.sport.blog.dao.GeneralDAO;
+import com.sport.blog.util.HibernateUtil;
+
+public abstract class GeneralDaoImpl<E> implements GeneralDAO<E> {
+
+	public void addElement(E element) {
+		Session session = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			Transaction transaction = session.beginTransaction();
+			session.save(element);
+			transaction.commit();
+		} finally {
+			if ((session != null) && (session.isOpen())) {
+				session.close();
+			}
+		}
+	}
+
+	public void updateElement(E element) {
+		Session session = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			Transaction transaction = session.beginTransaction();
+			session.update(element);
+			transaction.commit();
+		} finally {
+			if ((session != null) && (session.isOpen())) {
+				session.close();
+			}
+		}
+	}
+
+	public E getElementByID(Long elementId) {
+		Session session = null;
+		E element = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			element = (E) session.get(getClass(), elementId);
+		} finally {
+			if ((session != null) && (session.isOpen())) {
+				session.close();
+			}
+		}
+		return element;
+	}
+
+	public List<E> getAllElements() {
+		Session session = null;
+		List<E> elements = new ArrayList<E>();
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			elements = session.createCriteria(getClass()).list();
+		} finally {
+			if ((session != null) && (session.isOpen())) {
+				session.close();
+			}
+		}
+		return elements;
+	}
+
+	public void deleteElement(E element) {
+		Session session = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			Transaction transaction = session.beginTransaction();
+			session.delete(element);
+			transaction.commit();
+		} finally {
+			if ((session != null) && (session.isOpen())) {
+			}
+		}
+	}
+}
