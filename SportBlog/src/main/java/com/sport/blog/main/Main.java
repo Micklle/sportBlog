5122f2c70b2b2cc50694fc0e1d477e40e3635c0e
Oleg Masnik <olegmasnik@gmail.com>
2015-05-25 23:16:08 +0300

    Описав класи Юзер і Роль. Описав дао інтерфейси і їх реалізував. Запустив програму для перевірки чи все працює. Своє дз я вже зробив. Ви повинні тепер написати і реалізувати дао інтерфейси для свого завдання на подобі у пакетах dao i daoImpl того як я зробив та описати кліси своїх таблиць у пакеті model. Після цього можете переробити Маін клас під себе і перевірити своє завдання.

--- SportBlog/src/main/java/com/sport/blog/main/Main.java
+++ SportBlog/src/main/java/com/sport/blog/main/Main.java
@@ -1,9 +1,15 @@
 package com.sport.blog.main;
 
+import com.sport.blog.daoImpl.UserDaoImpl;
+import com.sport.blog.model.User;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		UserDaoImpl userDaoImpl = new UserDaoImpl();
+		
+		User user = new User("Oleg", "454545");
+		userDaoImpl.addElement(user);
+		System.out.println("Finish");
 	}
-
 }
