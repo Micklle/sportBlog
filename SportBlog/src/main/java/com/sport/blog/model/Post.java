90a2683d9c0936654d7d16a7efb804b5ebe2f79f
Oleg Masnik <olegmasnik@gmail.com>
2015-05-27 18:41:24 +0300

    refresh Posr and HashTag

--- SportBlog/src/main/java/com/sport/blog/model/Post.java
+++ SportBlog/src/main/java/com/sport/blog/model/Post.java
@@ -9,8 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Id;
 
