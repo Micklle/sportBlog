3074e8bcd553de97e45dca4c26809d37d31ccdeb
Micklle <mholovanov@gmail.com>
2015-06-09 23:37:54 +0300

    ..

--- SportBlog/src/main/java/com/sport/blog/daoImpl/HashTagDaoImpl.java
+++ SportBlog/src/main/java/com/sport/blog/daoImpl/HashTagDaoImpl.java
@@ -2,6 +2,8 @@ package com.sport.blog.daoImpl;
 
 import java.io.Serializable;
 
+import javax.transaction.Transactional;
+
 import org.hibernate.Session;
 
 import com.sport.blog.dao.HashTagDAO;
@@ -15,6 +17,7 @@ public class HashTagDaoImpl extends GeneralDaoImpl<HashTag>  implements HashTagD
 		super(HashTag.class);
 		// TODO Auto-generated constructor stub
 	}
+	@Transactional
 	public HashTag getHashTagByName(String name) {
 		return (HashTag) entityManager.createQuery("select u from HashTag as u where u.name = ?").setParameter(0, name).getSingleResult();
 	}
--- SportBlog/src/main/java/com/sport/blog/daoImpl/PostDaoImpl.java
+++ SportBlog/src/main/java/com/sport/blog/daoImpl/PostDaoImpl.java
@@ -2,6 +2,8 @@ package com.sport.blog.daoImpl;
 
 import java.io.Serializable;
 
+import javax.transaction.Transactional;
+
 import org.hibernate.Hibernate;
 import org.hibernate.Session;
 
@@ -16,11 +18,11 @@ public class PostDaoImpl extends GeneralDaoImpl <Post> implements PostDAO{
 		super(Post.class);
 		// TODO Auto-generated constructor stub
 	}
-
+	@Transactional
 	public Post getPostByText(String name) {
 		return (Post) entityManager.createQuery("select u from Post as u where u.name = ?").setParameter(0, name).getSingleResult();
 	}
-
+	@Transactional
 	public Post getPostByTitle(String title) {
 		return (Post) entityManager.createQuery("select u from user as u where u.title = ?").setParameter(0, title).getSingleResult();
 	}
