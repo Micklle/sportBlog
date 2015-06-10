48765c41668753719dbd2e6041d458906c6e33ac
Oleg Masnik <olegmasnik@gmail.com>
2015-06-10 23:04:41 +0300

    good

--- SportBlog/src/main/java/com/sport/blog/controler/PostController.java
+++ SportBlog/src/main/java/com/sport/blog/controler/PostController.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,21 +14,22 @@ import com.sport.blog.service.PostService;
 @Controller
 @RequestMapping("/posts")
 public class PostController {
+	
 	@Inject
 	private PostService postservice;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String getPostsByTitle(Model model) {
+	public String getPostsByTitle(Model model, @PathVariable String title) {
 
-		model.addAttribute("posts", postservice.getPostByTitle());
+		model.addAttribute("posts", postservice.getPostByTitle(title));
 		return "posts";
 
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String getPostsByText(Model model) {
+	public String getPostsByText(Model model,  @PathVariable String text) {
 
-		model.addAttribute("posts", postservice.getPostByText());
+		model.addAttribute("posts", postservice.getPostByText(text));
 		return "posts";
 	}
 
